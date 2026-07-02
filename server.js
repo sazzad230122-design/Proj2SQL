2a2ae558741b7d7a6308c15522c67255d624a5ca
Md Sazzad Hossen <sazzad.230122@s.pust.ac.bd>
2026-07-02 22:27:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-
 require("./models/db");
-
 const studentRoutes = require("./routes/studentRoutes");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 app.use("/students", studentRoutes);
-
 app.get("/", (req, res) => {
   res.send("Student Management SQL API Running...");
 });
-
 const PORT = 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
