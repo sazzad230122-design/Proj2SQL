5a0ce9228e0664c9e99261a1b2dda9ba3441d7f4
Md Sazzad Hossen <sazzad.230122@s.pust.ac.bd>
2026-07-02 22:28:21 +0600

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,18 +1,15 @@
 const mysql = require("mysql2");
-
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Sajib+230122",
-  database: "student_management",
+host: "localhost",
+user: "root",
+password: "Sajib+230122",
+database: "student_management",
 });
-
 db.connect((err) => {
   if (err) {
-    console.error("Database Connection Failed:", err);
+   console.error("Database Connection Failed:", err);
   } else {
     console.log("MySQL Connected Successfully");
   }
 });
-
 module.exports = db;
